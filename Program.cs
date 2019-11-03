8013d05e26bdde918435f473d50faf3869b115e6
André Ricardo <andric87@gmail.com>
2019-11-03 22:48:53 +0000

    Changes for dotnet 3.0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,7 +14,45 @@ namespace dotnet_v3
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var envs = System.Environment.GetEnvironmentVariables();
+
+            var webhostBuilder = CreateHostBuilder(args);
+
+            SetupAppRunnerEnvironment(webhostBuilder, envs);
+
+            webhostBuilder.Build().Run();
+        }
+
+        public static void SetupAppRunnerEnvironment(IHostBuilder hostBuilder, IDictionary envs)
+        {
+            if (envs.Contains("APP_PORT"))
+            {
+                var port = int.Parse(envs["APP_PORT"].ToString());
+
+                hostBuilder.ConfigureWebHost(webhostBuilder =>
+                    webhostBuilder.UseKestrel(options =>
+                    {
+                        options.ListenAnyIP(port);
+                    })
+                );
+            }
+
+            if (envs.Contains("APP_NAME"))
+            {
+                //app runner expect the service to be host at http(s)://host:port/APP_NAME
+                hostBuilder.ConfigureHostConfiguration(config =>
+                    {
+                        config.AddInMemoryCollection(new List<KeyValuePair<string, string>>() {
+                        new KeyValuePair<string, string>("PathBase", envs["APP_NAME"].ToString())
+                        });
+                    })
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                       webBuilder.UseStartup<Startup>();
+                    });
+//                var configration = builder.Build();
+                //webhostBuilder.UseConfiguration(configration);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
